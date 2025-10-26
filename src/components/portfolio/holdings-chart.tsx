23a7359b4ba607536a564bd024aa9e3355b8a2ec
Adarsh Arun <adarsharun99@gmail.com>
2025-10-26 00:54:48 +0000

    The pie chart looks much better! Make sure each stock has it's own uniqu

--- src/components/portfolio/holdings-chart.tsx
+++ src/components/portfolio/holdings-chart.tsx
@@ -18,18 +18,11 @@ const chartConfig = {
   },
   ...Object.fromEntries(
     holdingsData.map((holding, i) => [
-      holding.symbol,
+      holding.name, // Use name as the key for consistency
       { label: holding.name, color: `hsl(var(--chart-${i + 1}))` },
     ])
   ),
 } satisfies ChartConfig;
-
-const COLORS = chartConfig
-  ? Object.values(chartConfig)
-      .map((entry) => (entry.color ? entry.color : null))
-      .filter((color): color is string => color !== null)
-      .slice(1) // remove first "Holdings" color
-  : [];
 
 export function HoldingsChart() {
   const totalValue = React.useMemo(() => {
@@ -68,8 +61,11 @@ export function HoldingsChart() {
             innerRadius={60}
             strokeWidth={5}
           >
-            {holdingsData.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            {holdingsData.map((entry) => (
+              <Cell 
+                key={`cell-${entry.name}`} 
+                fill={chartConfig[entry.name]?.color} 
+              />
             ))}
           </Pie>
           <ChartLegend
